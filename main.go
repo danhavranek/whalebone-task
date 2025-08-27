1ec89b84e3f46fe049f7897f3f65092984d07512
Daniel Havranek <117735733+danhavranek@users.noreply.github.com>
2025-08-27 12:59:28 +0200

    refactor(api): create routes package

--- main.go
+++ main.go
@@ -1,55 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/danhavranek/whalebone-task/database"
-	"github.com/danhavranek/whalebone-task/models"
-	"github.com/google/uuid"
+	"github.com/danhavranek/whalebone-task/routes"
 )
 
-func savePerson(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	var newPerson models.Person
-	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&newPerson)
-	if err != nil {
-		panic(err)
-	}
-	// TODO: save data into DB
-	w.WriteHeader(http.StatusOK)
-}
-
-func getPerson(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	// fmt.Fprintf(w, "%s\n", strings.Trim(req.URL.Path, "/"))
-	// TODO: keep original offset in timestamp
-	personMock := models.Person{ExternalId: uuid.NewString(), Name: "Test Person", Email: "test@example.com", DateOfBirth: time.Now().UTC().Format(time.RFC3339)}
-	data, err := json.Marshal(personMock)
-	if err != nil {
-		// TODO: handle panic branches as server errors
-		panic(err)
-	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "%s\n", string(data))
-}
-
-func initializeRoutes() {
-	http.HandleFunc("/save", savePerson)
-	http.HandleFunc("/", getPerson)
-}
-
 func main() {
-	initializeRoutes()
+	routes.Init()
 
 	if err := database.Init(); err != nil {
 		panic(err)
---
+++ routes/routes.go
@@ -0,0 +1,48 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/danhavranek/whalebone-task/models"
+	"github.com/google/uuid"
+)
+
+func savePerson(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var newPerson models.Person
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&newPerson)
+	if err != nil {
+		panic(err)
+	}
+	// TODO: save data into DB
+	w.WriteHeader(http.StatusOK)
+}
+
+func getPerson(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// fmt.Fprintf(w, "%s\n", strings.Trim(req.URL.Path, "/"))
+	// TODO: keep original offset in timestamp
+	personMock := models.Person{ExternalId: uuid.NewString(), Name: "Test Person", Email: "test@example.com", DateOfBirth: time.Now().UTC().Format(time.RFC3339)}
+	data, err := json.Marshal(personMock)
+	if err != nil {
+		// TODO: handle panic branches as server errors
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", string(data))
+}
+
+func Init() {
+	http.HandleFunc("/save", savePerson)
+	http.HandleFunc("/", getPerson)
+}
