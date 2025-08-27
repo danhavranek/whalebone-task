c219f8461e86be44a8ebd5f937a4e4628842f409
Daniel Havranek <117735733+danhavranek@users.noreply.github.com>
2025-08-27 10:29:25 +0200

    feat(api): fallback route

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -13,8 +14,17 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
+func getPerson(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", strings.Trim(req.URL.Path, "/"))
+}
+
 func initializeRoutes() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/", getPerson)
 }
 
 func main() {
