be8c5db90baf560100778b9b513a11546ea5337d
Daniel Havranek <117735733+danhavranek@users.noreply.github.com>
2025-08-28 20:47:44 +0200

    chore: add comment

--- models/person.go
+++ models/person.go
@@ -1,5 +1,12 @@
 package models
 
+// Since the service doesn't work with the data internally,
+// all fields of the Person struct are stored as strings.
+// Validation is handled in ../routes/routes.go.
+// This decision allows us to preserve the original RFC3339
+// time offset format without having to create methods
+// to solve this problem.
+
 type Person struct {
 	// TODO: add internal id?
 	// Id          uint   `json:"-" gorm:"primaryKey"`
